b29110c349bb40d05faf8d5f36b9b39564fd5ac3
Your Name <vsis@vsis.com>
2022-10-19 17:07:08 +0800

    finish eval

--- onpolicy/scripts/eval_catching_both.sh
+++ onpolicy/scripts/eval_catching_both.sh
@@ -6,14 +6,23 @@ num_agents=4
 algo="rmappo"
 exp="debug"
 seed_max=1
-model_dir="/workspace/on-policy/onpolicy/scripts/results/MPE/simple_catching_expert_both/rmappo/Imitation_48_egocentric_one-mask_flexible_motion_rotation_lre-5/wandb/run-20221009_170852-28goz1nv/files"
-load_model_ep=200
-num_test_episode=10
+model_dir="/workspace/tmp/pretrained_data"
+load_model_ep=900
+num_test_episode=200
 step_mode="expert_adversary" # assert mode == expert_adversary or  expert_both or  expert_prey or  none
-
+step_mode_1="expert_prey"
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
+
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=0 python eval/eval_catching_both.py --use_render True --render_episodes 10 --save_gifs True --use_wandb False --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 1 --num_mini_batch 1 --episode_length 200 --num_env_steps 6000000 --ppo_epoch 10 --use_ReLU --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --model_dir ${model_dir} --load_model_ep ${load_model_ep} --num_test_episode ${num_test_episode} --step_mode ${step_mode}
+    echo "step_mode is ${step_mode_1}:"
+    CUDA_VISIBLE_DEVICES=0 python eval/eval_catching_both.py --use_render True --render_episodes 10 --save_gifs False --use_wandb False --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 1 --num_mini_batch 1 --episode_length 200 --num_env_steps 6000000 --ppo_epoch 10 --use_ReLU --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --model_dir ${model_dir} --load_model_ep ${load_model_ep} --num_test_episode ${num_test_episode} --step_mode ${step_mode_1}
+done
+
+for seed in `seq ${seed_max}`;
+do
+    echo "seed is ${seed}:"
+    echo "step_mode is ${step_mode}:"
+    CUDA_VISIBLE_DEVICES=0 python eval/eval_catching_both.py --use_render True --render_episodes 10 --save_gifs False --use_wandb False --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 1 --num_mini_batch 1 --episode_length 200 --num_env_steps 6000000 --ppo_epoch 10 --use_ReLU --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --model_dir ${model_dir} --load_model_ep ${load_model_ep} --num_test_episode ${num_test_episode} --step_mode ${step_mode}
 done
--- onpolicy/scripts/train_catching_both_based_on_pre.sh
+++ onpolicy/scripts/train_catching_both_based_on_pre.sh
@@ -14,5 +14,5 @@ echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, ma
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=1 python train/train_catching_both.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 10 --num_mini_batch 8 --episode_length 200 --num_env_steps 10000000 --ppo_epoch 8 --use_ReLU --gain 0.01 --lr 1e-5 --critic_lr 7e-4 --wandb_name "joe-14807" --user_name "joe-14807" --model_dir ${model_dir} --load_model_ep ${load_model_ep} --step_mode ${step_mode}
+    CUDA_VISIBLE_DEVICES=1 python train/train_catching_both.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 10 --num_mini_batch 8 --episode_length 200 --num_env_steps 20000000 --ppo_epoch 8 --use_ReLU --gain 0.01 --lr 1e-5 --critic_lr 7e-4 --wandb_name "joe-14807" --user_name "joe-14807" --model_dir ${model_dir} --load_model_ep ${load_model_ep} --step_mode ${step_mode}
 done
