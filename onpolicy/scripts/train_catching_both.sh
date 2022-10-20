054d153a5365048bde01268259b3bab5ed02abd9
vsis16 <hao.zhang@mail.sdu.edu.cn>
2022-10-20 21:37:53 +0800

    finish eval

--- onpolicy/scripts/train_catching_both.sh
+++ onpolicy/scripts/train_catching_both.sh
@@ -4,12 +4,12 @@ scenario="simple_catching_expert_both"  # simple_speaker_listener # simple_refer
 num_landmarks=0
 num_agents=4
 algo="rmappo"
-exp="EnvV1_check_48_egocentric_one-mask_flexible_motion_rotation_Expert_both"
+exp="EnvV2_TargetSpeed1.0_Egocentric_One-Mask_48_ExpertPrey"
 seed_max=1
-
+step_mode="expert_prey" # assert mode == expert_adversary or  expert_both or  expert_prey or  none
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=1 python train/train_catching_both.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 20 --num_mini_batch 4 --episode_length 100 --num_env_steps 10000000 --ppo_epoch 8 --use_ReLU --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "joe-14807" --user_name "joe-14807"
+    CUDA_VISIBLE_DEVICES=1 python train/train_catching_both.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 16 --num_mini_batch 8 --episode_length 200 --num_env_steps 20000000 --ppo_epoch 8 --use_ReLU --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "joe-14807" --user_name "joe-14807" --step_mode ${step_mode} 
 done
